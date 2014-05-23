e8a0147257f02f8dc68fd9277232b4fbfe62abe5
SST3ALBISG <SST3ALBISG@MAE217-01.ads.ntu.ac.uk>
2014-05-23 18:47:47 +0100

    DisplayGrid: tidied up functions and created new functions to minimise unnecessary steps, added JavaDoc. LabelPoint: list function moved from DisplayGrid. visualinfo: added package-info.

--- WifiPositioning/BaseLibrary/src/main/java/datastorage/LabelPoint.java
+++ WifiPositioning/BaseLibrary/src/main/java/datastorage/LabelPoint.java
@@ -4,7 +4,12 @@
  */
 package datastorage;
 
+import filehandling.RoomInfo;
 import general.Point;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 
 /**
  *
@@ -19,5 +24,22 @@ public class LabelPoint extends Point {
         this.label = label;
     }
         
+    public static List<LabelPoint> list(final HashMap<String, ? extends Location> radioMap, final HashMap<String, RoomInfo> roomInfo) {
+
+        List<LabelPoint> labelPoints = new ArrayList<>();
+
+        Set<String> keys = radioMap.keySet();
+
+        for (String key : keys) {
+            Location point = radioMap.get(key);
+            if (roomInfo.containsKey(point.getRoom())) {
+
+                Point pos = RoomInfo.searchPoint(point, roomInfo);
+                labelPoints.add(new LabelPoint(pos.getXint(), pos.getYint(), point.getXYStr()));
+            }
+        }
+
+        return labelPoints;
+    }
     
 }
--- WifiPositioning/BaseLibrary/src/main/java/visualinfo/DisplayGrid.java
+++ WifiPositioning/BaseLibrary/src/main/java/visualinfo/DisplayGrid.java
@@ -4,7 +4,6 @@
  */
 package visualinfo;
 
-import datastorage.LabelPoint;
 import datastorage.Location;
 import datastorage.RSSIData;
 import filehandling.RoomInfo;
@@ -19,79 +18,132 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
 import javax.imageio.ImageIO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * Overlay locations onto an image.
  *
  * @author Greg Albiston
  */
 public class DisplayGrid {
+
+    private static final int xOffset = 4;
+    private static final int yOffset = 12;
+    private static final int fontSize = 8;
+    private static final int pointWidth = 4;
+    private static final int pointHeight = 4;
+    private static final int halfPointWidth = pointWidth / 2;
+    private static final int halfPointHeight = pointHeight / 2;
+
     
     private static final Logger logger = LoggerFactory.getLogger(DisplayGrid.class);
-    
-    public static void print(File workingPath, File floorPlan, List<RSSIData> rssiDataList, HashMap<String, RoomInfo> roomInfo, boolean isRoomOutlineDraw) {
 
+    /**
+     * Draw list of locations onto an image, called DataGrid.png, and store the
+     * new image on the specified file path. Use default colours of red for
+     * points and blue for room outlines.
+     *
+     * @param workingPath Path to store the image.
+     * @param filename Name of file to print, without file extension.
+     * @param floorPlanFile Floor plan image to draw upon.
+     * @param rssiDataList List of locations to draw.
+     * @param roomInfo Information about the location coordinates.
+     * @param isRoomOutlineDraw True, draw outline of rooms in roomInfo.
+     */
+    public static void print(File workingPath, String filename, File floorPlanFile, List<RSSIData> rssiDataList, HashMap<String, RoomInfo> roomInfo, boolean isRoomOutlineDraw) {
+        render(floorPlanFile, rssiDataList, roomInfo, isRoomOutlineDraw, Color.RED, Color.BLUE);
+    }
+
+    /**
+     * Draw list of locations onto an image, called DataGrid.png, and store the
+     * new image on the specified file path.
+     *
+     * @param workingPath Path to store the image.
+     * @param filename Name of file to print, without file extension.
+     * @param floorPlanFile Floor plan image to draw upon.
+     * @param rssiDataList List of locations to draw.
+     * @param roomInfo Information about the location coordinates.
+     * @param isRoomOutlineDraw True, draw outline of rooms in roomInfo.
+     * @param pointColour Colour to draw points.
+     * @param roomColour Colour to draw room outlines.
+     */
+    public static void print(File workingPath, String filename, File floorPlanFile, List<RSSIData> rssiDataList, HashMap<String, RoomInfo> roomInfo, boolean isRoomOutlineDraw, Color pointColour, Color roomColour) {
+
+        String fullFileName = filename + ".png";
         try {
-            HashMap<String, Location> dataPoints = new HashMap<>();
-            for (RSSIData rssiData : rssiDataList) {
 
-                if (!dataPoints.containsKey(rssiData.getRoomRef())) //New entry created and added
-                {
-                    dataPoints.put(rssiData.getRoomRef(), rssiData);
-                }
-            }
-          
             //Add in colour selection.
-            BufferedImage gridImage = DisplayGrid.render(floorPlan, dataPoints, roomInfo, Color.RED, isRoomOutlineDraw, Color.BLUE);
-            File outputfile = new File(workingPath, "DataGrid.png");
+            BufferedImage gridImage = render(floorPlanFile, rssiDataList, roomInfo, isRoomOutlineDraw, pointColour, roomColour);
+            File outputfile = new File(workingPath, fullFileName);
             ImageIO.write(gridImage, "png", outputfile);
-        } catch (IOException ex) {            
-            logger.error("Error saving DataGrid.png file");
+        } catch (IOException ex) {
+            logger.error(String.format("Error saving %s file: {}", fullFileName), ex);
         }
     }
-    
-    public static BufferedImage render(final File floorPlanFile, final HashMap<String, Location> radioMap, final HashMap<String, RoomInfo> roomInfo, Color pointColour, boolean isRoomOutlineDraw, Color roomColour) {
+
+    /**
+     * Draw list of locations onto an image, called DataGrid.png, and store the
+     * new image on the specified file path.
+     *
+     * @param floorPlanFile Floor plan image to draw upon.
+     * @param radioMap Map of locations to draw.
+     * @param roomInfo Information
+     * @param isRoomOutlineDraw
+     * @param pointColour Colour to draw points.
+     * @param roomColour Colour to draw room outlines.
+     * @return
+     */
+    public static BufferedImage render(final File floorPlanFile, final HashMap<String, Location> radioMap, final HashMap<String, RoomInfo> roomInfo, boolean isRoomOutlineDraw, Color pointColour, Color roomColour) {
+        return render(floorPlanFile, new ArrayList<>(radioMap.values()), roomInfo, isRoomOutlineDraw, pointColour, roomColour);
+    }
+
+    /**
+     * Draw list of locations onto an image, called DataGrid.png, and store the
+     * new image on the specified file path.
+     *
+     * @param floorPlanFile Floor plan image to draw upon.
+     * @param locationList List of locations to draw.
+     * @param roomInfo Information
+     * @param isRoomOutlineDraw
+     * @param pointColour Colour to draw points.
+     * @param roomColour Colour to draw room outlines.
+     * @return
+     */
+    public static BufferedImage render(final File floorPlanFile, final List<? extends Location> locationList, final HashMap<String, RoomInfo> roomInfo, boolean isRoomOutlineDraw, Color pointColour, Color roomColour) {
 
         BufferedImage floorPlanImage = null;
-        
+
         try {
             floorPlanImage = ImageIO.read(floorPlanFile);
             Graphics2D floorPlan = floorPlanImage.createGraphics();
-            
-            floorPlan.setFont(new Font("Serif", Font.PLAIN, 8));
-            
-            if(isRoomOutlineDraw)
-            {
+
+            floorPlan.setFont(new Font("Serif", Font.PLAIN, fontSize));
+
+            if (isRoomOutlineDraw) {
                 floorPlan.setColor(roomColour);
-                Set<String> roomKeys =roomInfo.keySet();
-                for(String roomKey: roomKeys)
-                {
-                    Rectangle roomRect = roomInfo.get(roomKey).getRoomRect();
+
+                List<RoomInfo> rooms = new ArrayList<>(roomInfo.values());
+                for (RoomInfo room : rooms) {
+                    Rectangle roomRect = room.getRoomRect();
                     floorPlan.drawRect(roomRect.x, roomRect.y, roomRect.width, roomRect.height);
-                    String text = roomInfo.get(roomKey).getName();
-                    floorPlan.drawString(text, roomRect.x + 4, roomRect.y + 12);
+                    floorPlan.drawString(room.getName(), roomRect.x + xOffset, roomRect.y + yOffset);
                 }
             }
-                
-            
+
             floorPlan.setColor(pointColour);
-            //Draw each location point
-            Set<String> keys =radioMap.keySet();
-                  
-            for(String key: keys)
-            {
-                Location point = radioMap.get(key);
+            //Draw each location point  
+
+            for (Location point : locationList) {
                 if (roomInfo.containsKey(point.getRoom())) {
                     String text = point.getXYStr();
 
-                    Point pos = findPoint(roomInfo, point); 
-                    floorPlan.fillOval(pos.getXint() - 2, pos.getYint() - 2, 4, 4);
-                    floorPlan.drawString(text, pos.getXint() - 4, pos.getYint() - 4);
+                    Point pos = RoomInfo.searchPoint(point, roomInfo);
+                    floorPlan.fillOval(pos.getXint() - halfPointWidth, pos.getYint() - halfPointHeight, pointWidth, pointHeight);
+                    floorPlan.drawString(text, pos.getXint() - xOffset, pos.getYint() - yOffset);
                 }
-            }                        
+            }
 
             floorPlan.dispose();
             return floorPlanImage;
@@ -99,31 +151,6 @@ public class DisplayGrid {
             logger.error(ex.getMessage());
         }
         return floorPlanImage;
-    }          
-    
-    public static List<LabelPoint> list(final HashMap<String, ? extends Location> radioMap, final HashMap<String, RoomInfo> roomInfo) {
-
-        List<LabelPoint> labelPoints = new ArrayList<>();
-                    
-            Set<String> keys =radioMap.keySet();
-            
-            for(String key: keys)
-            {
-                Location point = radioMap.get(key);
-                if (roomInfo.containsKey(point.getRoom())) {
-                                        
-                    Point pos = findPoint(roomInfo, point); 
-                    labelPoints.add(new LabelPoint(pos.getXint(), pos.getYint(), point.getXYStr()));
-                }
-            }                        
-      
-        return labelPoints;
     }
-    
-    public static Point findPoint(final HashMap<String, RoomInfo> roomInfo, Location location){
-        
-        RoomInfo room = roomInfo.get(location.getRoom());
-        return room.getPoint(location.getxRef(), location.getyRef());                 
-    }      
-    
+
 }

1cc2eb83c0dba5a7e0f13a5b9ce3af1e4ef004a8
SST3ALBISG <SST3ALBISG@MAE217-01.ads.ntu.ac.uk>
2014-05-23 19:01:27 +0100

    DisplayGrid: re-organised function signatures to be consistent with other classes, added JavaDoc.

--- WifiPositioning/BaseLibrary/src/main/java/visualinfo/DisplayRoute.java
+++ WifiPositioning/BaseLibrary/src/main/java/visualinfo/DisplayRoute.java
@@ -18,42 +18,68 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * Draws paths of points onto an image.
  *
  * @author Greg Albiston
  */
 public class DisplayRoute {
 
     private static final Logger logger = LoggerFactory.getLogger(DisplayRoute.class);
-    
+
     private final static int HALF_SIZE = 4;
-    private final static int SIZE = HALF_SIZE*2;
-    private final static int CIRCLE_SIZE = HALF_SIZE*3;
-    private final static int HALF_CIRCLE_SIZE = CIRCLE_SIZE/2;
-    
+    private final static int SIZE = HALF_SIZE * 2;
+    private final static int CIRCLE_SIZE = HALF_SIZE * 3;
+    private final static int HALF_CIRCLE_SIZE = CIRCLE_SIZE / 2;
+
     private final static int POINT_WIDTH = 1;
     private final static int LINE_WIDTH = 3;
-    private final static Color TRIAL_COLOR = Color.RED;
-    private final static Color FINAL_COLOR = Color.BLUE;       
-    
-    public static void draw(List<Point> trialPoints, List<Point> finalPoints, File routeFile, File floorPlanFile) {
-      
+
+    /**
+     * Draws the trial and final routes onto the specified image and stores in
+     * the specified file. Defaults colour red for trial path and blue for final
+     * path.
+     *
+     * @param workingPath File to store image.
+     * @param filename Name of file to print, without file extension.
+     * @param floorPlanFile Image file to draw upon.     
+     * @param trialPoints List of points in the trial.
+     * @param finalPoints List of points in final estimated points.
+     */
+    public static void draw(File workingPath, String filename, File floorPlanFile, List<Point> trialPoints, List<Point> finalPoints) {
+        draw(workingPath, filename, floorPlanFile, trialPoints, finalPoints, Color.RED, Color.BLUE);
+    }
+
+    /**
+     * Draws the trial and final routes onto the specified image and stores in
+     * the specified file.
+     *
+     * @param workingPath Path to store the image.
+     * @param filename Name of file to print, without file extension.   
+     * @param floorPlanFile Image file to draw upon.
+     * @param trialPoints List of points in the trial.
+     * @param finalPoints List of points in final estimated points.workingPath@param routeFile File to store image.
+     * @param trialColor Colour of trial path.
+     * @param finalColor Colour of final path.
+     */
+    public static void draw(File workingPath, String filename, File floorPlanFile, List<Point> trialPoints, List<Point> finalPoints, Color trialColor, Color finalColor) {
+
         try {
             BufferedImage floorPlanImage = ImageIO.read(floorPlanFile);
             Graphics2D floorPlan = floorPlanImage.createGraphics();
-          
+
             floorPlan.setFont(new Font("Arial", Font.PLAIN, 12));
-            
-            floorPlan.setColor(TRIAL_COLOR);
+
+            floorPlan.setColor(trialColor);
             drawLines(floorPlan, trialPoints);
             drawPoints(floorPlan, trialPoints);
 
-            floorPlan.setColor(FINAL_COLOR);
+            floorPlan.setColor(finalColor);
             drawLines(floorPlan, finalPoints);
             drawPoints(floorPlan, finalPoints);
 
             floorPlan.dispose();
 
-            File outputfile = new File(routeFile.getPath() + ".png");
+            File outputfile = new File(workingPath, filename + ".png");
             ImageIO.write(floorPlanImage, "png", outputfile);
 
         } catch (IOException ex) {
@@ -73,15 +99,15 @@ public class DisplayRoute {
         }
 
         //First point a triangle and last point is drawn with an oval.
-        if(!points.isEmpty()){
-            
+        if (!points.isEmpty()) {
+
             //First Point
             Point firstPoint = points.get(0);
-            int x[] = {firstPoint.getXint()-SIZE,firstPoint.getXint(),firstPoint.getXint()+SIZE};
-            int y[] = {firstPoint.getYint()+SIZE,firstPoint.getYint()-SIZE,firstPoint.getYint()+SIZE};
-            floorPlan.fillPolygon(x,y,3);
+            int x[] = {firstPoint.getXint() - SIZE, firstPoint.getXint(), firstPoint.getXint() + SIZE};
+            int y[] = {firstPoint.getYint() + SIZE, firstPoint.getYint() - SIZE, firstPoint.getYint() + SIZE};
+            floorPlan.fillPolygon(x, y, 3);
             floorPlan.drawString(String.valueOf(0), firstPoint.getXint() - HALF_SIZE, firstPoint.getYint() - SIZE);
-            
+
             //Last Point
             Point lastPoint = points.get(points.size() - 1);
             floorPlan.fillOval(lastPoint.getXint() - HALF_CIRCLE_SIZE, lastPoint.getYint() - HALF_CIRCLE_SIZE, CIRCLE_SIZE, CIRCLE_SIZE);
@@ -90,12 +116,12 @@ public class DisplayRoute {
     }
 
     private final static float dash[] = {10.0f};
-    private final static BasicStroke dashed =
-        new BasicStroke(LINE_WIDTH,
-                        BasicStroke.CAP_BUTT,
-                        BasicStroke.JOIN_MITER,
-                        10.0f, dash, 0.0f);
-    
+    private final static BasicStroke dashed
+            = new BasicStroke(LINE_WIDTH,
+                    BasicStroke.CAP_BUTT,
+                    BasicStroke.JOIN_MITER,
+                    10.0f, dash, 0.0f);
+
     //draws lines between points in the list
     private static void drawLines(Graphics2D floorPlan, List<Point> points) {
 

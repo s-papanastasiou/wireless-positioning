856aa2fadd65651f05770a02e95254101468d371
Greg Albiston <greg_albiston@hotmail.com>
2014-09-06 16:35:29 +0100

    KNNFloorPoint: matching attributes changed to handle testing of RSSI and Geomagnetic with separate values.

--- WifiPositioning/KNNLibrary/src/main/java/datastorage/KNNFloorPoint.java
+++ WifiPositioning/KNNLibrary/src/main/java/datastorage/KNNFloorPoint.java
@@ -6,7 +6,9 @@ package datastorage;
 
 import general.AvgValue;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -19,10 +21,10 @@ import java.util.Set;
  *
  */
 public class KNNFloorPoint extends Location implements Serializable {
-
+    
     private HashMap<String, AvgValue> attributes = new HashMap<>();
     private final String roomReference;
-
+    
     public final static int NO_ORIENTATION = -1;
 
     /**
@@ -67,7 +69,7 @@ public class KNNFloorPoint extends Location implements Serializable {
         this.attributes.put(key, new AvgValue(value));
         this.roomReference = super.getRoomRef();
     }
-
+    
     public KNNFloorPoint(final Location location, final String key, final Integer value) {
         super(location);
         this.attributes.put(key, new AvgValue(value));
@@ -129,34 +131,34 @@ public class KNNFloorPoint extends Location implements Serializable {
      */
     public KNNFloorPoint(final KNNFloorPoint floorPoint) {
         super(floorPoint);
-
+        
         this.attributes = new HashMap<>();
         for (String key : floorPoint.attributes.keySet()) {
             AvgValue value = new AvgValue(floorPoint.attributes.get(key));
             this.attributes.put(key, value);
         }
-
+        
         this.roomReference = floorPoint.roomReference;
     }
-
+    
     public HashMap<String, AvgValue> getAttributes() {
         return attributes;
     }
-
+    
     public String printAttributes() {
         String results = "";
-
+        
         Set<String> keys = attributes.keySet();
         for (String key : keys) {
             AvgValue value = attributes.get(key);
             results += key + " " + value.toString() + System.lineSeparator();
         }
-
+        
         return results;
     }
-
+    
     public void add(final String key, final Double value) {
-
+        
         if (attributes.containsKey(key)) {
             AvgValue avgValue = attributes.get(key);
             avgValue.add(value);
@@ -164,13 +166,13 @@ public class KNNFloorPoint extends Location implements Serializable {
             attributes.put(key, new AvgValue(value));
         }
     }
-
+    
     public void add(final String key, final Integer value) {
         add(key, value.doubleValue());
     }
-
+    
     public void add(HashMap<String, AvgValue> attributes) {
-
+        
         for (String key : attributes.keySet()) {
             AvgValue value = attributes.get(key);
             if (this.attributes.containsKey(key)) {
@@ -178,24 +180,24 @@ public class KNNFloorPoint extends Location implements Serializable {
                 avgValue.add(value);
             } else {
                 attributes.put(key, new AvgValue(value));
-            }            
+            }
         }
     }
-
+    
     public void add(final String key, final Integer value, final Boolean isBSSIDMerged) {
         add(key, value.doubleValue(), isBSSIDMerged);
     }
 
     //Variant that strips the key (BSSID) down from six hex pairs to five.
     public void add(final String key, final Double value, final Boolean isBSSIDMerged) {
-
+        
         int beginIndex = 0;
         int endIndex = 17; //full BSSID - six hex pairs
 
         if (isBSSIDMerged) {
             endIndex = 14;
         }
-
+        
         String bssid = key.substring(beginIndex, endIndex);  //Copy out the BSSID based on whether merging or not.                           
         if (attributes.containsKey(bssid)) {
             AvgValue avgValue = attributes.get(bssid);
@@ -204,19 +206,19 @@ public class KNNFloorPoint extends Location implements Serializable {
             attributes.put(bssid, new AvgValue(value));
         }
     }
-
+    
     @Override
     public String getRoomRef() {
         return roomReference;
     }
-
+    
     public static HashMap<String, KNNFloorPoint> filterMap(HashMap<String, KNNFloorPoint> offlineMap, int orientation) {
         HashMap<String, KNNFloorPoint> orientatedMap;
-
+        
         if (orientation != NO_ORIENTATION) {
             orientatedMap = new HashMap<>();
             Set<String> keys = offlineMap.keySet();
-
+            
             for (String key : keys) {
                 KNNFloorPoint orientatedPoint = offlineMap.get(key);
                 if (orientatedPoint.getwRef() == orientation) {
@@ -226,38 +228,62 @@ public class KNNFloorPoint extends Location implements Serializable {
         } else {
             orientatedMap = offlineMap;
         }
-
+        
         return orientatedMap;
     }
-
+    
     public Boolean equivalentTo(KNNFloorPoint other) {
-
+        
         Boolean result = true;
-
+        
         HashMap<String, AvgValue> otherAttributes = other.attributes;
         if (this.attributes.size() == otherAttributes.size()) {
-
+            
             for (String key : this.attributes.keySet()) {
                 if (!otherAttributes.containsKey(key)) {
                     result = false;
                     break;
                 }
             }
-
+            
         } else {
             result = false;
         }
-
+        
         return result;
-    }
-
-    public Boolean matchingAttributes(KNNFloorPoint other, Double tolerance) {
-        Boolean result = true;
-
+    }    
+    
+    public Boolean matchingAttributes(KNNFloorPoint other, Double rssiTolerance, Double geoTolerance) {
+        Boolean result;
+        
         HashMap<String, AvgValue> otherAttributes = other.attributes;
         if (this.attributes.size() == otherAttributes.size()) {
-
-            for (String key : this.attributes.keySet()) {
+            //Test the geo keys against tolerance
+            result = testAttributes(GeomagneticData.KEY_LIST, this.attributes, otherAttributes, geoTolerance);
+            
+            if (result) {
+                //Test the RSSI keys against tolerance
+                List<String> rssiKeys = new ArrayList<>();
+                for (String key : this.attributes.keySet()) {
+                    
+                    if (!GeomagneticData.KEY_LIST.contains(key)) {
+                        rssiKeys.add(key);
+                    }
+                }
+                result = testAttributes(rssiKeys, this.attributes, otherAttributes, rssiTolerance);
+            }
+        } else {
+            result = false;
+        }
+        
+        return result;
+    }
+    
+    private static Boolean testAttributes(List<String> keys, HashMap<String, AvgValue> attributes, HashMap<String, AvgValue> otherAttributes, Double tolerance) {
+        
+        Boolean result = true;
+        for (String key : keys) {
+            if (attributes.containsKey(key)) {
                 if (otherAttributes.containsKey(key)) {
                     Double mean = attributes.get(key).getMean();
                     Double otherMean = otherAttributes.get(key).getMean();
@@ -270,47 +296,43 @@ public class KNNFloorPoint extends Location implements Serializable {
                     break;
                 }
             }
-
-        } else {
-            result = false;
         }
-
         return result;
     }
-
+    
     public String toStringAttributes(String fieldSeparator) {
-
+        
         StringBuilder stb = new StringBuilder();
         for (String key : attributes.keySet()) {
             stb.append(key).append(" ").append(attributes.get(key)).append(fieldSeparator);
         }
         return stb.substring(0, stb.length() - 1);
     }
-    
+
     /**
      * Merges two HashMaps of KNNFloorPoints together - deep copy
-     * 
+     *
      * @param firstFloorPoints
      * @param secondFloorPoints
-     * @return 
+     * @return
      */
-    public static HashMap<String, KNNFloorPoint> merge(HashMap<String, KNNFloorPoint> firstFloorPoints, HashMap<String, KNNFloorPoint> secondFloorPoints){
+    public static HashMap<String, KNNFloorPoint> merge(HashMap<String, KNNFloorPoint> firstFloorPoints, HashMap<String, KNNFloorPoint> secondFloorPoints) {
         
         HashMap<String, KNNFloorPoint> mergedFloorPoints = new HashMap<>();
         
-        for(String key: firstFloorPoints.keySet()){
+        for (String key : firstFloorPoints.keySet()) {
             KNNFloorPoint value = new KNNFloorPoint(firstFloorPoints.get(key));
             mergedFloorPoints.put(key, value);
         }
         
-        for(String key: secondFloorPoints.keySet()){
+        for (String key : secondFloorPoints.keySet()) {
             
             KNNFloorPoint value = secondFloorPoints.get(key);
             
-            if(mergedFloorPoints.containsKey(key)){                
-                KNNFloorPoint mergedValue = mergedFloorPoints.get(key);                
+            if (mergedFloorPoints.containsKey(key)) {
+                KNNFloorPoint mergedValue = mergedFloorPoints.get(key);
                 mergedValue.add(value.getAttributes());
-            }else{
+            } else {
                 KNNFloorPoint newValue = new KNNFloorPoint(value);
                 mergedFloorPoints.put(key, newValue);
             }
@@ -318,5 +340,5 @@ public class KNNFloorPoint extends Location implements Serializable {
         
         return mergedFloorPoints;
     }
-
+    
 }

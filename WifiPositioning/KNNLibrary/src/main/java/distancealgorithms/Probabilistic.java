c6aa6d2e13a23db4b295bdf3913bca2157dd383c
Greg Albiston <greg_albiston@hotmail.com>
2014-09-07 16:07:03 +0100

    Probabilistic: fixes to get working.

--- WifiPositioning/KNNLibrary/src/main/java/distancealgorithms/Probabilistic.java
+++ WifiPositioning/KNNLibrary/src/main/java/distancealgorithms/Probabilistic.java
@@ -17,16 +17,16 @@ import org.slf4j.LoggerFactory;
 public class Probabilistic {
 
     private static final Logger logger = LoggerFactory.getLogger(Probabilistic.class);
-             
+
     public static Location run(KNNFloorPoint onlinepoint, HashMap<String, KNNFloorPoint> offlineMap, HashMap<String, RoomInfo> roomInfo, int kLimit, int orientation) {
 
         Location location;
 
         if (!onlinepoint.getAttributes().isEmpty()) {
-                         
-            HashMap<String, KNNFloorPoint> orientatedMap = KNNFloorPoint.filterMap(offlineMap, orientation);            
+
+            HashMap<String, KNNFloorPoint> orientatedMap = KNNFloorPoint.filterMap(offlineMap, orientation);
             TreeSet<ResultLocation> results = new TreeSet<>();
-            
+
             Set<String> keys = orientatedMap.keySet();
 
             for (String key : keys) {
@@ -49,19 +49,18 @@ public class Probabilistic {
         }
 
         return location;
-    }   
+    }
 
     private static Point position(List<ResultLocation> kList) {
         double x = 0;
         double y = 0;
         double sum = 0;
 
-        for (ResultLocation result : kList) {            
+        for (ResultLocation result : kList) {
             x += result.getResult() * result.getGlobalX();
             y += result.getResult() * result.getGlobalY();
             sum += result.getResult();
         }
-
 
         return new Point(x / sum, y / sum);
     }
@@ -91,36 +90,52 @@ public class Probabilistic {
 
         return p;
     }
-    
+
     //Distance equations based on http://www.statsoft.com/textbook/k-nearest-neighbors/
-    public static double distance( final HashMap<String, AvgValue> trialAttributes, final HashMap<String, AvgValue> offlineAttributes) {
-                
-        final double NO_MATCH = 0.01;
-        final double sqrPI = Math.sqrt(2 * Math.PI);    
-        double distance=1.0f;
-        
+    /**
+     * @author Greg Albiston
+     * @param trialAttributes
+     * @param offlineAttributes
+     * @return
+     */
+    public static double distance(final HashMap<String, AvgValue> trialAttributes, final HashMap<String, AvgValue> offlineAttributes) {
+
+        final double NO_MATCH = Double.MIN_NORMAL;
+        final double sqrPI = Math.sqrt(2 * Math.PI);
+        double probTotal = 0.0;
+
         Set<String> keys = trialAttributes.keySet();
-        
-        for(String key: keys)
-        {
-            if(offlineAttributes.containsKey(key))
-            {
+
+        for (String key : keys) {
+            if (offlineAttributes.containsKey(key)) {
                 AvgValue offline = offlineAttributes.get(key);
                 AvgValue trial = trialAttributes.get(key);
-                if ((offline.getMean() != 0) && (offline.getVariance() > 0)) {
-                    double c = 1 / (offline.getStdDev() * sqrPI);
-                    double d = (trial.getMean() - offline.getMean());
-                    double powD = Math.pow(d, 2);
-                    double e = Math.exp(-powD / (2 * offline.getVariance()));
-                    distance *= (c * e);
-                }
-                
-            }else{ //poor distance given. otherwise points would be rewards for lacking data detected by the scan point.
-                distance*=NO_MATCH;
-            }                            
-        }        
-        distance = Math.sqrt(distance);                
-        
+
+                double diff = trial.getMean() - offline.getMean();
+                double sqrDiff = Math.pow(diff, 2);
+
+                if (offline.getVariance() == 0) { //No variance so only square difference has an effect
+                    probTotal += Math.exp(-sqrDiff);
+                } else {
+                    double a = 1 / (offline.getStdDev() * sqrPI);
+
+                    double b;
+
+                    if (sqrDiff != 0) { //zero difference means result is exponent 1
+                        b = Math.exp(-sqrDiff / (2 * offline.getVariance()));
+                    } else {
+                        b = 1;
+                    }
+                    double prob = (a * b);
+                    probTotal += prob;
+                }                
+
+            } else { //poor distance given. otherwise points would be rewards for lacking data detected by the trial point.
+                probTotal += NO_MATCH;
+            }
+        }
+
+        double distance = probTotal / keys.size();
         return distance;
     }
 }

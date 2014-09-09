16816894a00aaa6267b8758c60d810554ae21300
Greg Albiston <greg_albiston@hotmail.com>
2014-09-09 16:44:03 +0100

    Probabilistic: NO_MATCH changed back again.

--- WifiPositioning/KNNLibrary/src/main/java/distancealgorithms/Probabilistic.java
+++ WifiPositioning/KNNLibrary/src/main/java/distancealgorithms/Probabilistic.java
@@ -100,7 +100,7 @@ public class Probabilistic {
      */
     public static double distance(final HashMap<String, AvgValue> trialAttributes, final HashMap<String, AvgValue> offlineAttributes) {
 
-        final double NO_MATCH = 0;
+        final double NO_MATCH = Math.exp(-100);
         final double sqrPI = Math.sqrt(2 * Math.PI);
         double probTotal = 0.0;
 
